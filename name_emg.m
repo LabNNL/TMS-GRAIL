0e6a616093b7ff50749c8d00b802cd6e7eba27d7
ant-coh <antoine.cohic@gmail.com>
2026-07-17 09:18:29 -0400

    Added function to rename the EMG fields

---
+++ name_emg.m
@@ -0,0 +1,14 @@
+function emg = name_emg(emg,tab_assoc)
+
+% emg : struct
+% tab_assoc : cell array
+
+for i=1:size(tab_assoc,1)
+    if ~isempty(tab_assoc{i,2})
+        field_sensor=['Sensor', num2str(tab_assoc{i,2})];
+        emg.(tab_assoc{i,1})=emg.(field_sensor);
+        emg=rmfield(emg,field_sensor);
+    end
+end
+
+end
