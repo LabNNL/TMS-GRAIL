5a6295bfdf929b0d1b76419919bab5aeb85944c1
ant-coh <antoine.cohic@gmail.com>
2026-07-20 14:22:16 -0400

    Renamed input to "struc" to avoid potential issues

--- get_crp.m
+++ get_crp.m
@@ -1,66 +1,58 @@
-function [crp_struct] = get_crp(struct)
+function [crp_struc] = get_crp(struc)
 
 side={'Left','Right'};
 angle={'Hip','Knee','Ankle'};
-pad=.1*struct.Frequency;
+pad=.1*struc.Frequency;
 
-fc=6; fs=struct.Frequency;
+fc=6; fs=struc.Frequency;
 [b,a]=butter(2,2*fc/fs,'low');
 
 for j=1:2
 
-    hs_lab=[side{j} '_Foot_Strike'];                                        % HS and TO subfields
-    to_lab=[side{j} '_Foot_Off'];
-    nbc=length(struct.Events.(hs_lab))-1;
-    HS=round(struct.Events.(hs_lab)*struct.Frequency-struct.FirstFrame+1);  % A check
-    TO=round(struct.Events.(to_lab)*struct.Frequency-struct.FirstFrame+1);
+    hs_lab=[side{j} '_Foot_Strike'];
+    nbc=length(struc.Events.(hs_lab))-1;
+    HS=round(struc.Events.(hs_lab)*struc.Frequency-struc.FirstFrame+1);
     crp_kh_temp=[];
     crp_ak_temp=[];
     ind=1;
 
     for cy=1:nbc
 
-        TOcy=TO(TO>HS(cy) & TO<HS(cy+1));
-        if isempty(TOcy)
-            crp_struct.(side{j}).toe_off_timing(cy,1)=0;
-        else
-            crp_struct.(side{j}).toe_off_timing(cy,1)=((TOcy-HS(cy))/(HS(cy+1)-HS(cy)))*100;
-        end
         lb=HS(cy)-pad;
         ub=HS(cy+1)+pad;
 
-        if lb<1 || ub>struct.LastFrame-struct.FirstFrame+1                  % padding out of bounds
+        if lb<1 || ub>struc.LastFrame-struc.FirstFrame+1                    % padding out of bounds
             continue
         else
             for ang=1:length(angle)
                 ang_lab=[side{j}(1),angle{ang},'Angles'];
-                temp_filt=filtfilt(b,a,struct.Angles.(ang_lab));
+                temp_filt=filtfilt(b,a,struc.Angles.(ang_lab));
                 temp=temp_filt(lb:ub,1);
-                crp_struct.(side{j}).cycles_padded.(angle{ang}){cy,1}=temp';
+                crp_struc.(side{j}).cycles_padded.(angle{ang}){cy,1}=temp';
                 PA_temp=Phase_Angle(temp);
-                crp_struct.(side{j}).phase_angles.(angle{ang}){cy,1}=PA_temp(pad+1:end-pad,1)';
+                crp_struc.(side{j}).phase_angles.(angle{ang}){cy,1}=PA_temp(pad+1:end-pad,1)';
             end
 
-            h_ang=crp_struct.(side{j}).phase_angles.Hip{cy,1};
-            k_ang=crp_struct.(side{j}).phase_angles.Knee{cy,1};
-            a_ang=crp_struct.(side{j}).phase_angles.Ankle{cy,1};
+            h_ang=crp_struc.(side{j}).phase_angles.Hip{cy,1};
+            k_ang=crp_struc.(side{j}).phase_angles.Knee{cy,1};
+            a_ang=crp_struc.(side{j}).phase_angles.Ankle{cy,1};
             temp=CRP(k_ang,h_ang);
             crp_norm=interp1(linspace(1,size(temp,2),size(temp,2)),temp,linspace(1,size(temp,2),101));
-            crp_struct.(side{j}).CRP_cycles.Knee_Hip{cy,1}=crp_norm;
+            crp_struc.(side{j}).CRP_cycles.Knee_Hip{cy,1}=crp_norm;
             crp_kh_temp(ind,:)=crp_norm;
             temp=CRP(a_ang,k_ang);
             crp_norm=interp1(linspace(1,size(temp,2),size(temp,2)),temp,linspace(1,size(temp,2),101));
-            crp_struct.(side{j}).CRP_cycles.Ankle_Knee{cy,1}=crp_norm;
+            crp_struc.(side{j}).CRP_cycles.Ankle_Knee{cy,1}=crp_norm;
             crp_ak_temp(ind,:)=crp_norm;
             ind=ind+1;
 
         end
     end
 
-    crp_struct.(side{j}).MARP.Knee_Hip=mean(crp_kh_temp,1);
-    crp_struct.(side{j}).MARP.Ankle_Knee=mean(crp_ak_temp,1);
-    crp_struct.(side{j}).DP.Knee_Hip=std(crp_kh_temp,0,1);
-    crp_struct.(side{j}).DP.Ankle_Knee=std(crp_ak_temp,0,1);
+    crp_struc.(side{j}).MARP.Knee_Hip=mean(crp_kh_temp,1);
+    crp_struc.(side{j}).MARP.Ankle_Knee=mean(crp_ak_temp,1);
+    crp_struc.(side{j}).DP.Knee_Hip=std(crp_kh_temp,0,1);
+    crp_struc.(side{j}).DP.Ankle_Knee=std(crp_ak_temp,0,1);
 
 end
 
--- get_mos.m
+++ get_mos.m
@@ -1,22 +1,22 @@
-function [mos_struct] = get_mos(struct)
+function [mos_struc] = get_mos(struc)
 
 side={'Left','Right'};
-fs=struct.Frequency;
+fs=struc.Frequency;
 fc=6;                                                                       % Low-pass at 6 Hz
 
 % Centre of mass
 
 [b,a]=butter(2,2*fc/fs,'low');
-com_ap=filtfilt(b,a,-struct.CentreOfMass(:,2)');                            % In the direction of progression
-com_v=filtfilt(b,a,struct.CentreOfMass(:,3)');                              % See lab coordinate system
-vcom_ap=gradient(com_ap,1/fs)+struct.Misc.WalkingSpeed*1000;
+com_ap=filtfilt(b,a,-struc.CentreOfMass(:,2)');                            % In the direction of progression
+com_v=filtfilt(b,a,struc.CentreOfMass(:,3)');                              % See lab coordinate system
+vcom_ap=gradient(com_ap,1/fs)+struc.Misc.WalkingSpeed*1000;
 
 for j=1:2
     
     if j==1
-        com_ml=filtfilt(b,a,struct.CentreOfMass(:,1)');                     % From medial to lateral
+        com_ml=filtfilt(b,a,struc.CentreOfMass(:,1)');                     % From medial to lateral
     else
-        com_ml=filtfilt(b,a,-struct.CentreOfMass(:,1)');  
+        com_ml=filtfilt(b,a,-struc.CentreOfMass(:,1)');  
     end
     vcom_ml=gradient(com_ml,1/fs);
 
@@ -24,14 +24,14 @@ for j=1:2
 
     hs_lab=[side{j} '_Foot_Strike'];                                        % HS and TO subfields
     to_lab=[side{j} '_Foot_Off'];
-    nbc=length(struct.Events.(hs_lab))-1;
-    HS=round(struct.Events.(hs_lab)*struct.Frequency-struct.FirstFrame+1);  % A check
-    TO=round(struct.Events.(to_lab)*struct.Frequency-struct.FirstFrame+1);
+    nbc=length(struc.Events.(hs_lab))-1;
+    HS=round(struc.Events.(hs_lab)*struc.Frequency-struc.FirstFrame+1);
+    TO=round(struc.Events.(to_lab)*struc.Frequency-struc.FirstFrame+1);
     
     % Eigenfrequency of pendulum
 
     jc_lab=[side{j}(1),'AJC'];                                              % Ankle joint center
-    ajc_v=filtfilt(b,a,struct.Misc.(jc_lab)(:,3)');
+    ajc_v=filtfilt(b,a,struc.Misc.(jc_lab)(:,3)');
     temp_tab=[];
     ind=1;                                                                  % Mean height difference between CoM
     for cy=1:nbc                                                            % and the ankle axis during stance
@@ -45,27 +45,27 @@ for j=1:2
         end
     end
     L0=mean(temp_tab,'all');                                                % Effective pendulum length
-    mos_struct.(side{j}).L0=L0;
+    mos_struc.(side{j}).L0=L0;
     omega_0=sqrt(9806.65/L0);                                               % g in mm/s^2
 
     % Extrapolated centre of mass
 
     xcom_ap=com_ap+vcom_ap/omega_0;
     xcom_ml=com_ml+vcom_ml/omega_0;
-    mos_struct.(side{j}).XcoM_AP=xcom_ap;
-    mos_struct.(side{j}).XcoM_ML=xcom_ml;
+    mos_struc.(side{j}).XcoM_AP=xcom_ap;
+    mos_struc.(side{j}).XcoM_ML=xcom_ml;
 
     % Markers
 
     heel_lab=[side{j}(1),'HEE'];
     toe_lab=[side{j}(1),'TOE'];
     vmh_lab=[side{j}(1),'VMH'];
-    heel_ap=filtfilt(b,a,-struct.Markers.(heel_lab)(:,2)');
-    toe_ap=filtfilt(b,a,-struct.Markers.(toe_lab)(:,2)');
+    heel_ap=filtfilt(b,a,-struc.Markers.(heel_lab)(:,2)');
+    toe_ap=filtfilt(b,a,-struc.Markers.(toe_lab)(:,2)');
     if j==1
-        vmh_ml=filtfilt(b,a,struct.Markers.(vmh_lab)(:,1)');
+        vmh_ml=filtfilt(b,a,struc.Markers.(vmh_lab)(:,1)');
     else
-        vmh_ml=filtfilt(b,a,-struct.Markers.(vmh_lab)(:,1)');
+        vmh_ml=filtfilt(b,a,-struc.Markers.(vmh_lab)(:,1)');
     end
     
     % Margin of stability
@@ -82,9 +82,9 @@ for j=1:2
             temp_heel=heel_ap(HS(cy):TOcy)-xcom_ap(HS(cy):TOcy);
             temp_toe=toe_ap(HS(cy):TOcy)-xcom_ap(HS(cy):TOcy);
             temp_vmh=vmh_ml(HS(cy):TOcy)-xcom_ml(HS(cy):TOcy);
-            mos_struct.(side{j}).MoS_AP_Heel.Cycles{cy,1}=temp_heel;
-            mos_struct.(side{j}).MoS_AP_Toe.Cycles{cy,1}=temp_toe;
-            mos_struct.(side{j}).MoS_ML.Cycles{cy,1}=temp_vmh;
+            mos_struc.(side{j}).MoS_AP_Heel.Cycles{cy,1}=temp_heel;
+            mos_struc.(side{j}).MoS_AP_Toe.Cycles{cy,1}=temp_toe;
+            mos_struc.(side{j}).MoS_ML.Cycles{cy,1}=temp_vmh;
             
             len=size(temp_heel,2);
             mos_ap_heel_temp(ind,:)=interp1(linspace(1,len,len),temp_heel,linspace(1,len,101));
@@ -98,16 +98,16 @@ for j=1:2
     
     leg_lab=[side{j}(1),'LegLength'];
     atd_lab=[side{j}(1),'AsisTrocanterDistance'];
-    trocanter_height=struct.Misc.(leg_lab)-struct.Misc.(atd_lab);
-    mos_struct.(side{j}).TrocanterHeight=trocanter_height;
+    trocanter_height=struc.Misc.(leg_lab)-struc.Misc.(atd_lab);
+    mos_struc.(side{j}).TrocanterHeight=trocanter_height;
 
-    mos_struct.(side{j}).MoS_AP_Heel.NormalizedMean=mean(mos_ap_heel_temp,1)/trocanter_height;
-    mos_struct.(side{j}).MoS_AP_Toe.NormalizedMean=mean(mos_ap_toe_temp,1)/trocanter_height;
-    mos_struct.(side{j}).MoS_ML.NormalizedMean=mean(mos_ml_temp,1)/trocanter_height;
+    mos_struc.(side{j}).MoS_AP_Heel.NormalizedMean=mean(mos_ap_heel_temp,1)/trocanter_height;
+    mos_struc.(side{j}).MoS_AP_Toe.NormalizedMean=mean(mos_ap_toe_temp,1)/trocanter_height;
+    mos_struc.(side{j}).MoS_ML.NormalizedMean=mean(mos_ml_temp,1)/trocanter_height;
 
-    mos_struct.(side{j}).MoS_AP_Heel.NormalizedStd=std(mos_ap_heel_temp,0,1)/trocanter_height;
-    mos_struct.(side{j}).MoS_AP_Toe.NormalizedStd=std(mos_ap_toe_temp,0,1)/trocanter_height;
-    mos_struct.(side{j}).MoS_ML.NormalizedStd=std(mos_ml_temp,0,1)/trocanter_height;
+    mos_struc.(side{j}).MoS_AP_Heel.NormalizedStd=std(mos_ap_heel_temp,0,1)/trocanter_height;
+    mos_struc.(side{j}).MoS_AP_Toe.NormalizedStd=std(mos_ap_toe_temp,0,1)/trocanter_height;
+    mos_struc.(side{j}).MoS_ML.NormalizedStd=std(mos_ml_temp,0,1)/trocanter_height;
 
 end
 
